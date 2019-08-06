fc10614ea2004e0e7a92ee07df54dbe5ec32a24b
GOCE1982 <goce.ilievski@live.com>
2019-08-06 02:29:48 +0200

    modify redux store

--- src/store.js
+++ src/store.js
@@ -6,6 +6,6 @@ const initalState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initalState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(rootReducer, initalState (applyMiddleware(...middleware)));
 
 export default store;
